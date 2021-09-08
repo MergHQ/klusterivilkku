83c6d1945fbf0f34585b2ed011effa7e281dde46
Hugo Holmqvist <hugo.holm@live.com>
2021-09-08 14:45:31 +0000

    Project base

---
+++ migrations/1631111870593_initial.sql
@@ -0,0 +1,21 @@
+-- Up Migration
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "citext";
+
+CREATE TABLE users (
+  id             UUID        PRIMARY KEY DEFAULT uuid_generate_v4(),
+  email          CITEXT      NOT NULL UNIQUE,
+  password       TEXT        NOT NULL,      
+  created_at     TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE check_ins (
+  id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id       UUID NOT NULL,
+  checked_in_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- Down Migration
